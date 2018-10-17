cc7097c9f298f9933d75bee7c92495fdad9ad103
achliopa <athchliopanos@gmail.com>
2018-10-17 19:51:22 +0300

    all

--- CourseRepo/routes/uploadRoutes.js
+++ CourseRepo/routes/uploadRoutes.js
@@ -1,13 +1,20 @@
 const AWS = require('aws-sdk');
 const keys = require('../config/keys');
+const uuid = require('uuid/v1');
+const requireLogin = require('../middlewares/requireLogin');
 
-const S3 = new AWS.S3({
+const s3 = new AWS.S3({
 	accessKeyId: keys.accessKeyId,
 	secretAccessKey: keys.secretAccessKey
 });
 
 module.exports = app => {
-	app.get('/api/upload', (req,res)=>{
-
+	app.get('/api/upload', requireLogin, (req,res)=>{
+		const key = `${req.user.id}/${uuid()}.jpeg`;
+		s3.getSignedUrl('putobject', {
+			Bucket: 'agileng-blog-bucket',
+			ContentType: 'image/jpeg',
+			Key: key
+		}, ()=>res.send({key, url}));
 	});
 };
